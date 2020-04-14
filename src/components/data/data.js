70fd83d1e7ef89b3c357d8fe55de3355bdc893f3
John Stilwell <johnrstilwell@codeimmersives.com>
2020-04-14 09:38:22 -0400

    animal arr separated

---
+++ src/components/data/data.js
@@ -0,0 +1,88 @@
+let animals = [
+    {
+        image: '/images/dog1.jpg',
+        type: 'dog',
+        name: 'Battle',
+        description: 'This is a picture of a DOG',
+        animalId: 'doggo1',
+    },
+    {
+        image: '/images/dog2.jpg',
+        type: 'dog',
+        name: 'Roamin',
+        description: 'This is a picture of a DOG',
+        animalId: 'doggo2',
+    },
+    {
+        image: '/images/dog3.jpg',
+        type: 'dog',
+        name: 'Brando',
+        description: 'This is a picture of a DOG',
+        animalId: 'doggo3',
+    },
+    {
+        image: '/images/dog4.jpg',
+        type: 'dog',
+        name: 'Max',
+        description: 'This is a picture of a DOG',
+        animalId: 'doggo4',
+    },
+    {
+        image: '/images/cat1.jpg',
+        type: 'cat',
+        name: 'Michelle',
+        description: 'This is a picture of a CAT',
+        animalId: 'kitty1',
+    },
+    {
+        image: '/images/cat2.jpg',
+        type: 'cat',
+        name: 'Dani',
+        description: 'This is a picture of a CAT',
+        animalId: 'kitty2',
+    },
+    {
+        image: '/images/cat3.jpg',
+        type: 'cat',
+        name: 'Sarah',
+        description: 'This is a picture of a CAT',
+        animalId: 'kitty3',
+    },
+    {
+        image: '/images/cat4.jpg',
+        type: 'cat',
+        name: 'Erin',
+        description: 'This is a picture of a CAT',
+        animalId: 'kitty4',
+    },
+    {
+        image: '/images/lion1.jpg',
+        type: 'lion',
+        name: 'Mufasa',
+        description: 'This is a picture of a LION',
+        animalId: 'roar1',
+    },
+    {
+        image: '/images/lion2.jpg',
+        type: 'lion',
+        name: 'Simba',
+        description: 'This is a picture of a LION',
+        animalId: 'roar2',
+    },
+    {
+        image: '/images/lion3.jpg',
+        type: 'lion',
+        name: 'Scar',
+        description: 'This is a picture of a LION',
+        animalId: 'roar3',
+    },
+    {
+        image: '/images/lion4.jpg',
+        type: 'lion',
+        name: 'Nala',
+        description: 'This is a picture of a LION',
+        animalId: 'roar4',
+    },
+];
+
+export default animals
